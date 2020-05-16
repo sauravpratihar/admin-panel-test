ecc63b829102c5b4bd501d5d74968c0227e497be
Saurav Pratihar <pratihar.saurav@gmail.com>
2020-05-17 03:37:42 +0530

    init commit

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Header = () => {
+  return <nav class="navbar is-fixed-top has-background-black"></nav>;
+};
+
+export default Header;
